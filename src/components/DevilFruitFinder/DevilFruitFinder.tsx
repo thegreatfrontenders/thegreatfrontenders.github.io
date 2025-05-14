499368ce827027ec137ac9a8e183fa9bf5920f4b
Genesis Bonds <genesis.bonds@gmail.com>
2025-05-14 00:14:34 -0400

    current

--- src/components/DevilFruitFinder/DevilFruitFinder.tsx
+++ src/components/DevilFruitFinder/DevilFruitFinder.tsx
@@ -1,12 +1,17 @@
-import React, { FC } from 'react';
+import React, { useState, useEffect, FC } from 'react';
 import styles from './DevilFruitFinder.module.css';
 
-interface DevilFruitFinderProps {}
 
-const DevilFruitFinder: FC<DevilFruitFinderProps> = () => (
-  <div className={styles.DevilFruitFinder} data-testid="DevilFruitFinder">
-    testing testing
-  </div>
-);
+interface DevilFruitFinderProps { }
+
+const DevilFruitFinder: FC<DevilFruitFinderProps> = () => {
+  const [numFruits, setNumFruits] = useState(10);
+
+  return (
+    <div className={styles.DevilFruitFinder} data-testid="DevilFruitFinder">
+
+    </div>
+  );
+}
 
 export default DevilFruitFinder;
